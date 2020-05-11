742d536022d59ae6cf933c9ce9a094c04c615bf5
Silverium <soldeplatadeveloper@gmail.com>
2020-05-11 17:44:44 +0200

    config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: 'jest',
+  extends: ['eslint:recommended', 'plugin:jest/recommended'],
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 'tab'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
